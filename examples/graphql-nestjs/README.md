6b1539ecd6e77c96fa94857ffd40bb3e73982caa
YutaSugimura <nextc3m@gmail.com>
2021-07-22 01:46:27 +0900

    add: examples/graphql-nestjs

---
+++ examples/graphql-nestjs/README.md
@@ -0,0 +1,22 @@
+# graphql-nestjs
+
+## setup
+
+1. install
+
+```zsh
+  npm ci
+```
+
+2. prisma
+
+```zsh
+  npx prisma migrate dev --name init
+  npx prisma db seed --preview-feature
+```
+
+3. start
+
+```zsh
+  npm run dev
+```
---
+++ examples/graphql-nestjs/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(3000, () => {
+    console.log(`
+🚀 Server ready at: http://localhost:3000/graphql
+⭐️ See sample queries: http://pris.ly/e/ts/graphql-nestjs#using-the-graphql-api
+`);
+  });
+}
+
+bootstrap().catch((e) => {
+  console.log({ error: e });
+});
